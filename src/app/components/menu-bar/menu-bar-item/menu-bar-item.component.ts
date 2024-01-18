dcb513dffd2b20ac6b37b96058a0688560fcca20
Roberta de Siqueira <designer.roberta@gmail.com>
2024-01-17 22:53:40 -0300

    change menu-bar-item

--- src/app/components/menu-bar/menu-bar-item/menu-bar-item.component.ts
+++ src/app/components/menu-bar/menu-bar-item/menu-bar-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar-item',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar-item.component.css']
 })
 export class MenuBarItemComponent {
+
+	@Input()
+	nameItem: string = '';
 
 }
